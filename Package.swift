a4b3a4194316b7efb2d9ecaa3218dfb017dcb636
Unpxre <unpxre.tw@gmail.com>
2022-02-17 15:41:50 +0800

    [add] 新增從泛型數值初始化 Data 結構方法

--- Package.swift
+++ Package.swift
@@ -7,11 +7,14 @@ let package = Package(
     name: "PracticalExtension",
     products: [
         .library(name: "PracticalExtension", targets: ["PracticalExtension"]),
+        .library(name: "PracticalSwiftExtension", targets: ["PracticalSwiftExtension"]),
+        .library(name: "PracticalFoundationExtension", targets: ["PracticalFoundationExtension"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "PracticalExtension", dependencies: ["PracticalSwiftExtension"]),
+        .target(name: "PracticalExtension", dependencies: ["PracticalSwiftExtension", "PracticalFoundationExtension"]),
         .target(name: "PracticalSwiftExtension"),
+        .target(name: "PracticalFoundationExtension"),
         .testTarget(name: "PracticalSwiftExtensionTest", dependencies: ["PracticalSwiftExtension"])
     ]
 )
