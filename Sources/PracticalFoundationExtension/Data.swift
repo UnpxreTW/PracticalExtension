344f16e3fe1db7924c4b7d7d6ec9266f7a8a62be
Unpxre <unpxre.tw@gmail.com>
2022-05-18 11:03:24 +0800

    [add] 新增快速轉換為 UInt8 字串陣列

--- Sources/PracticalFoundationExtension/Data.swift
+++ Sources/PracticalFoundationExtension/Data.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 public extension Data {
+    func printUInt8Array() -> String {
+        "[ " + map { String($0) }.joined(separator: ", ") + " ]"
+    }
+
     /// 使用任何型態數值初始化 Data 結構，並可以依據需求裁切大小或進行補零
     init<T>(from value: T, bySize size: Int? = nil, isBigEndian: Bool = false) {
         var data: Data = .init()
