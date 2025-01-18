96e009313d8394b567ccd8a10135c4578ae7d4c3
Unpxre <mail@unpxre.me>
2025-01-18 19:25:12 +0800

    clean: 專案結構清理 (#6)

--- Package.swift
+++ Package.swift
@@ -1,22 +1,18 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-    name: "PracticalExtension",
-    products: [
-        .library(name: "PracticalExtension", targets: ["PracticalExtension"]),
-        .library(name: "PracticalSwiftExtension", targets: ["PracticalSwiftExtension"]),
-        .library(name: "PracticalFoundationExtension", targets: ["PracticalFoundationExtension"]),
-        .library(name: "PracticalUIKitExtension", targets: ["PracticalUIKitExtension"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "PracticalExtension", dependencies: ["PracticalSwiftExtension", "PracticalFoundationExtension", "PracticalUIKitExtension"]),
-        .target(name: "PracticalSwiftExtension", dependencies: []),
-        .target(name: "PracticalFoundationExtension", dependencies: []),
-        .target(name: "PracticalUIKitExtension", dependencies: []),
-        .testTarget(name: "PracticalSwiftExtensionTest", dependencies: ["PracticalSwiftExtension"])
-    ]
+	name: "PracticalExtension",
+	products: [
+		.library(name: "PracticalSwiftExtension", targets: ["PracticalSwiftExtension"]),
+		.library(name: "PracticalFoundationExtension", targets: ["PracticalFoundationExtension"]),
+		.library(name: "PracticalUIKitExtension", targets: ["PracticalUIKitExtension"])
+	],
+	targets: [
+		.target(name: "PracticalSwiftExtension", dependencies: []),
+		.target(name: "PracticalFoundationExtension", dependencies: []),
+		.target(name: "PracticalUIKitExtension", dependencies: []),
+		.testTarget(name: "PracticalSwiftExtensionTest", dependencies: ["PracticalSwiftExtension"])
+	]
 )
